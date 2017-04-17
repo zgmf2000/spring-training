e0b422596e5ef3fdbfe6e1655c1431bfffcb34ad
Bintoro Adi Guna <trainee@MTPC216.mitrais.com>
2017-04-17 09:05:58 +0700

    Initial Commit

---
+++ src/main/java/com/mitrais/springtraining/SpringtrainingApplication.java
@@ -0,0 +1,12 @@
+package com.mitrais.springtraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtrainingApplication.class, args);
+	}
+}
